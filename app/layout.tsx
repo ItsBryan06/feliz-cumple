5d5805393bfe9f8c6349bcb76115c568b0267170
ItsBryan06 <36749794+ItsBryan06@users.noreply.github.com>
2025-06-06 11:57:06 -0500

    v0.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Feliz cumpleaños reina',
+  description: 'Para mi querida esposa <3',
+  generator: 'HollowRaven',
 }
 
 export default function RootLayout({
